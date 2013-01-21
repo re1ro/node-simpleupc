de031524075178ef97ec42d86d4729bb33adeb33
rma4ok <rma4ok@gmail.com>
2013-01-21 00:12:23 -0500

    [dist] grunt config

---
+++ grunt.js
@@ -0,0 +1,20 @@
+module.exports = function (grunt) {
+  "use strict";
+
+  grunt.loadNpmTasks('grunt-bg-shell');
+
+
+  // Project configuration.
+  grunt.initConfig({
+
+    bgShell: {
+      watchCoffee: {
+        cmd: './node_modules/coffee-script/bin/coffee -bw -o ./build -c ./src'
+      }
+    }
+
+  });
+
+
+  grunt.registerTask('default', 'bgShell');
+};
